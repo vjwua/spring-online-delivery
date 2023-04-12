f5c513a8b69495c05c752707f9efb903280049f9
vjwua <62188903+vjwua@users.noreply.github.com>
2023-04-12 11:17:41 +0300

    First commit

---
+++ src/main/java/com/pnudev/springonlinedelivery/SpringOnlineDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.pnudev.springonlinedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnlineDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOnlineDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnudev/springonlinedelivery/SpringOnlineDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnudev.springonlinedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOnlineDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
