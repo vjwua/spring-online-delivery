00f7a931856fc16faba683ec3793e3e49189508e
vjwua <62188903+vjwua@users.noreply.github.com>
2023-04-19 20:30:39 +0300

    Added repositories

---
+++ src/main/java/com/pnudev/springonlinedelivery/repos/MenuItemRepository.java
@@ -0,0 +1,8 @@
+package com.pnudev.springonlinedelivery.repos;
+
+import com.pnudev.springonlinedelivery.models.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuItemRepository {
+
+}
---
+++ src/main/java/com/pnudev/springonlinedelivery/repos/UserRepository.java
@@ -0,0 +1,8 @@
+package com.pnudev.springonlinedelivery.repos;
+
+import com.pnudev.springonlinedelivery.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository {
+
+}
