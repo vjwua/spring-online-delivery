c946ce1164342e533e1fd057b2bd6ce5c749bf0f
vjwua <ivasyuk359@gmail.com>
2023-04-29 12:50:09 +0300

    Little fixes on controllers

--- src/main/java/com/pnudev/springonlinedelivery/controllers/OrderController.java
+++ src/main/java/com/pnudev/springonlinedelivery/controllers/OrderController.java
@@ -1,17 +1,20 @@
 package com.pnudev.springonlinedelivery.controllers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pnudev.springonlinedelivery.models.Order;
 
+import java.util.Collections;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "/orders")
 public class OrderController {
     @GetMapping
     public List<Order> getOrders() {
-        return null;
+        return Collections.emptyList();
     }
 }
--- src/main/java/com/pnudev/springonlinedelivery/controllers/UserController.java
+++ src/main/java/com/pnudev/springonlinedelivery/controllers/UserController.java
@@ -1,17 +1,20 @@
 package com.pnudev.springonlinedelivery.controllers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pnudev.springonlinedelivery.models.User;
 
+import java.util.Collections;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
     @GetMapping
     public List<User> getUsers() {
-        return null;
+        return Collections.emptyList();
     }
 }
