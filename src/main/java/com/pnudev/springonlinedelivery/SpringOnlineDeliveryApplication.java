7559a35ca7d8f4189c63662bff87eb80080bd118
vjwua <vjwivasyuk@gmail.com>
2023-05-15 09:51:07 +0300

    Bad request fix

--- src/main/java/com/pnudev/springonlinedelivery/SpringOnlineDeliveryApplication.java
+++ src/main/java/com/pnudev/springonlinedelivery/SpringOnlineDeliveryApplication.java
@@ -3,7 +3,7 @@ package com.pnudev.springonlinedelivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.pnudev.springonlinedelivery.mapper")
+@SpringBootApplication
 public class SpringOnlineDeliveryApplication {
 
 	public static void main(String[] args) {
