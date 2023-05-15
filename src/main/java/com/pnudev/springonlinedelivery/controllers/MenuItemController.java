2e08a8ac21472617a83f907df538b782d2c05368
vjwua <vjwivasyuk@gmail.com>
2023-05-15 08:41:37 +0300

    PR corrections

--- src/main/java/com/pnudev/springonlinedelivery/controllers/MenuItemController.java
+++ src/main/java/com/pnudev/springonlinedelivery/controllers/MenuItemController.java
@@ -1,10 +1,11 @@
 package com.pnudev.springonlinedelivery.controllers;
 
 import com.pnudev.springonlinedelivery.dto.MenuItemDto;
-import com.pnudev.springonlinedelivery.repos.MenuItemRepository;
 import com.pnudev.springonlinedelivery.service.MenuItemService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -19,6 +20,4 @@ public class MenuItemController {
     public List<MenuItemDto> getMenuItems() {
         return menuItemService.getMenuItems();
     }
-
-    private MenuItemRepository menuItemRepository;
 }
--- src/main/java/com/pnudev/springonlinedelivery/service/MenuItemServiceImpl.java
+++ src/main/java/com/pnudev/springonlinedelivery/service/MenuItemServiceImpl.java
@@ -40,30 +40,5 @@ public class MenuItemServiceImpl implements MenuItemService {
         // TODO TBW
     }
 
-    public MenuItem dtoToEntity(MenuItemDto menuItemDto) {
-        if (menuItemDto == null) {
-            return null;
-        }
-        MenuItem menuItem = new MenuItem();
-        menuItem.setName(menuItemDto.getName());
-        menuItem.setSummary(menuItemDto.getSummary());
-        menuItem.setSize(menuItemDto.getSize());
-        menuItem.setPrice(menuItemDto.getPrice());
-        menuItem.setOrder(menuItemDto.getOrder());
-        return menuItem;
-    }
-
-    public MenuItemDto entityToDto(MenuItem menuItem) {
-        if (menuItem == null ) {
-            return null;
-        }
-        MenuItemDto menuItemDto = new MenuItemDto();
-        menuItemDto.setName(menuItem.getName());
-        menuItemDto.setSummary(menuItem.getSummary());
-        menuItemDto.setSize(menuItem.getSize());
-        menuItemDto.setPrice(menuItem.getPrice());
-        menuItemDto.setOrder(menuItem.getOrder());
-        return menuItemDto;
-    }
     // TODO Add implementation of the list of dishes
 }
