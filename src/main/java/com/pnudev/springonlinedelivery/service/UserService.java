2dd3a01cf178ac94d918c16a2591c3100ebbc1f2
vjwua <vjwivasyuk@gmail.com>
2023-05-21 15:56:46 +0300

    Revert "Added update API"
    This reverts commit 45a43211e8b1ff327259ef6e2d4ac36cdc2765f2.

--- src/main/java/com/pnudev/springonlinedelivery/service/UserService.java
+++ src/main/java/com/pnudev/springonlinedelivery/service/UserService.java
@@ -1,7 +1,6 @@
 package com.pnudev.springonlinedelivery.service;
 
 import com.pnudev.springonlinedelivery.dto.UserDto;
-import com.pnudev.springonlinedelivery.dto.UserUpdateDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +8,4 @@ import java.util.List;
 @Service
 public interface UserService {
     List<UserDto> getUsers();
-
-    UserDto updateUser(Long id, UserUpdateDto userUpdateDto);
 }
--- src/main/java/com/pnudev/springonlinedelivery/service/UserServiceImpl.java
+++ src/main/java/com/pnudev/springonlinedelivery/service/UserServiceImpl.java
@@ -1,14 +1,9 @@
 package com.pnudev.springonlinedelivery.service;
 
-import com.pnudev.springonlinedelivery.dto.OrderDto;
-import com.pnudev.springonlinedelivery.dto.OrderUpdateDto;
 import com.pnudev.springonlinedelivery.dto.UserDto;
-import com.pnudev.springonlinedelivery.dto.UserUpdateDto;
 import com.pnudev.springonlinedelivery.mapper.UserMapper;
-import com.pnudev.springonlinedelivery.models.Order;
 import com.pnudev.springonlinedelivery.models.User;
 import com.pnudev.springonlinedelivery.repos.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +15,6 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-
-    private static final String USER_MESSAGE = "User with ID %d is not found";
     @Override
     public List<UserDto> getUsers() {
         List<User> users = userRepository.findAll();
@@ -29,13 +22,5 @@ public class UserServiceImpl implements UserService {
         return users.stream()
                 .map(userMapper::entityToDto)
                 .toList();
-    }
-
-    @Override
-    public UserDto updateUser(Long id, UserUpdateDto userUpdateDto) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(USER_MESSAGE.formatted(id)));
-        userMapper.updateUserFromDto(userUpdateDto, user);
-        return userMapper.entityToDto(userRepository.save(user));
     }
 }
