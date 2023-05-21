b60a6b6dac338a7e026e921f8f2b4a5075ba7155
vjwua <vjwivasyuk@gmail.com>
2023-05-21 22:14:20 +0300

    Swagger implemented

--- src/main/java/com/pnudev/springonlinedelivery/controllers/OrderController.java
+++ src/main/java/com/pnudev/springonlinedelivery/controllers/OrderController.java
@@ -3,6 +3,11 @@ package com.pnudev.springonlinedelivery.controllers;
 import com.pnudev.springonlinedelivery.dto.OrderDto;
 import com.pnudev.springonlinedelivery.dto.OrderUpdateDto;
 import com.pnudev.springonlinedelivery.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,22 +28,48 @@ import java.util.List;
 public class OrderController {
     private final OrderService orderService;
 
+    @Operation(summary = "Get all orders")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found orders",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = OrderDto.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @GetMapping
     public List<OrderDto> getOrders() {
         return orderService.getOrders();
     }
 
+    @Operation(summary = "Add order to database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Order is added"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrderDto createOrder(@Valid @RequestBody OrderDto orderDto) {
         return orderService.createOrder(orderDto);
     }
 
+    @Operation(summary = "Update order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found and updated order",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = OrderDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Order is not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public OrderDto updateOrder(@PathVariable Long id, @RequestBody OrderUpdateDto orderUpdateDto){
         return orderService.updateOrder(id, orderUpdateDto);
     }
 
+    @Operation(summary = "Cancel order from database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Order is deleted"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @PutMapping(path = "/{id}/cancel")
     public void cancelOrder(@PathVariable Long id) {
         orderService.cancelOrder(id);
